af175df6860b0324d0a91df920dfb95280e46adf
vik802207 <137061206+vik802207@users.noreply.github.com>
2025-12-31 16:49:07 +0530

    Add login form UI

--- frontend/dashboard/src/api/axios.js
+++ frontend/dashboard/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://shopifyadmindashboard.onrender.com/api"
 });
 
 // 🔐 token auto attach
